fa1f5c0a293d2db1fb3d731a38eebda1b90b9961
Lewis Liu <lewisliuusa@outlook.com>
2021-02-09 16:48:48 -0800

    nav added

---
+++ GameStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace GameStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
