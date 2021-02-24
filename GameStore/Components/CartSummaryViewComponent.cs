c5012e97af4a36c3c1859be3e5e4f304ddb5d65b
Lewis Liu <lewisliuusa@outlook.com>
2021-02-23 22:09:21 -0800

    added shopping cart default page

---
+++ GameStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using GameStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
