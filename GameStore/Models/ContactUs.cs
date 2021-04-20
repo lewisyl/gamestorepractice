de2a5e8e6ac3d1fa6aedfda9d96a05fb27848a1b
Lewis Liu <lewisliuusa@outlook.com>
2021-04-20 08:49:02 -0700

    finished contact page view

--- GameStore/Models/ContactUs.cs
+++ GameStore/Models/ContactUs.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace GameStore.Models
 {
     public class ContactUs
     {
+        [BindNever]
+        public int CaseID { get; set; }
+
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your message")]
+        public string Message { get; set; }
     }
 }
