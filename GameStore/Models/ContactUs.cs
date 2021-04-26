8343811641ee4dc4a2a51474bc77313f510ea501
Lewis Liu <lewisliuusa@outlook.com>
2021-04-25 22:41:06 -0700

    added validations on name(letters only), email add.(has to have @ and .) and minLength for message

--- GameStore/Models/ContactUs.cs
+++ GameStore/Models/ContactUs.cs
@@ -13,12 +13,16 @@ namespace GameStore.Models
         public int CaseId { get; set; }
 
         [Required(ErrorMessage = "Please enter your name")]
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Use letters only please")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(@"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$", ErrorMessage = "Invalid Email Address")]
+        // [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Please enter your message")]
+        [MinLength(10,ErrorMessage = "Message should be at least 10 characters.")]
         public string Message { get; set; }
     }
 }
