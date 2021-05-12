7f274d357875ef75c8316d760080db071ea9fcd3
Lewis Liu <lewisliuusa@outlook.com>
2021-05-11 21:55:50 -0700

    added test actions for testing return types and context types

--- GameStore/Controllers/AboutUsController.cs
+++ GameStore/Controllers/AboutUsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameStore.Models;
+using GameStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -10,5 +12,14 @@ namespace GameStore.Controllers
     public class AboutUsController : Controller
     {
         public ViewResult Index() => View();
+
+        // Below are the test codes for 
+        public JsonResult TestJsonResult()
+        {
+            var sth = new ProductsListViewModel{
+                Products = _repo.Products
+            };
+            return Json(sth);
+        }
     }
 }
--- GameStore/Controllers/TestPageController.cs
+++ GameStore/Controllers/TestPageController.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameStore.Models;
+using GameStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameStore.Controllers
 {
     public class TestPageController : Controller
     {
+
+        private IProductRepository _repo;
+        public TestPageController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
         [Route("/testpage")]
         [Route("/testpage/index")]
         public IActionResult Index()
         {
-            return Content("Hi I'm Hein. Routing is successful.");
+            return Content("Hi, Lewis and Hein here. Routing is successful.");
+        }
+
+        public JsonResult TestJsonResult()
+        {
+            var sth = new ProductsListViewModel
+            {
+                Products = _repo.Products
+            };
+            return Json(sth);
         }
     }
 }
